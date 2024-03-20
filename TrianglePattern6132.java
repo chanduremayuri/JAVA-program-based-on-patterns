6ff3855a0acc50feb518749fb33c4434370f3130
Mayuri Chandure <118971104+chanduremayuri@users.noreply.github.com>
2024-03-20 21:28:02 +0530

    Add files via upload

---
+++ TrianglePattern6132.java
@@ -0,0 +1,21 @@
+
+class TrianglePattern6132 
+{
+	public static void main(String[] args) 
+	{
+		
+		int num = 0;
+		for(int i =1 ; i <=5 ;i++)
+		{
+			num = num + i;
+			for(int j =1 ; j <=i ; j++  )
+			{ 
+
+				System.out.print(num-- +" ");
+				
+			}
+			num = num + i;
+			System.out.println();
+		}
+	}
+}
---
+++ TrianglePattern_abc.java
@@ -0,0 +1,18 @@
+class TrianglePattern_abc {
+	public static void main(String[] args) 
+	{
+
+		
+		char ch ='a';
+		for(int i =1 ; i <= 5 ;i++)
+		{
+			
+			for( int j =1 ; j <= i ;j++)
+			{
+				System.out.print(ch++  + " ");
+			}
+			System.out.println();
+
+		}
+	}
+}
